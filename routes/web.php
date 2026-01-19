2961322617bd4df74362dd503d3ca59e7ba428c0
Christian06 <christian.quezada5968@utc.edu.ec>
2026-01-19 18:07:07 -0500

    auteticaciom Laravel Breeze + Livewire

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-use App\Http\Controllers\ClimaController;
+Route::view('/', 'welcome');
 
-Route::get('/clima', [ClimaController::class, 'index']);
-Route::get('/clima/eliminar/{id}', [ClimaController::class, 'eliminar']);
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
+
+require __DIR__.'/auth.php';
